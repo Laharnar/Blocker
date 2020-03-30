46689086f1f1cb8a5fa763c54647102786fb653f
Marko Laharnar <marko.laharnar@gmail.com>
2020-03-30 02:32:10 +0200

    Added UI connections for cost

---
+++ PilotSimulator/Assets/Scripts/Research/Maps/ResearchUICost.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ResearchUICost : MonoBehaviour {
+
+    public TMPro.TMP_Text text;
+    [SerializeField] string prefix = "";
+    [SerializeField] string suffix = "VSP";
+    [SerializeField] IntVarValue val;
+
+    private void Start()
+    {
+        DisplayCostWithSuffixAndPrefix();
+    }
+
+    public void Init(IntVar customVal)
+    {
+        Debug.Log("This is used [*2]. if not, remove it.");
+        val.SetIntPrefab(customVal);
+    }
+
+    [ContextMenu("Update cost text")]
+    public void DisplayCostWithSuffixAndPrefix()
+    {
+        text.text = "";
+        if (prefix != "")
+        {
+            text.text += prefix;
+        }
+        text.text += val.Value.ToString();
+        if (suffix != "")
+        {
+            text.text += suffix;
+        }
+    }
+}

b646db90b73fe81ea41cd8ee4717c8cb9bddca81
Marko Laharnar <marko.laharnar@gmail.com>
2020-04-08 19:18:17 +0200

    changed mono functions into prefab functions for collisions

--- PilotSimulator/Assets/Scripts/Unit/DamageSender.cs
+++ PilotSimulator/Assets/Scripts/Unit/DamageSender.cs
@@ -5,23 +5,8 @@ public class DamageSender : MonoBehaviour {
 
     public int damage;
 
-    // todo: remove these 3
-    public UnityEvent onDamage;
-    public bool pass = true;
-    public DamageReciever LastDamageOther { get; private set; }
-
     public void OnHit(DamageReciever d)
     {
-        // todo: remove these 4
-        LastDamageOther = d;
-        onDamage?.Invoke();
-        if (pass)
-        {
-
-            d.hp.RecieveDamage(damage);
-
-            // todo: remove these 2
-            pass = false;
-        }
+        d.hp.RecieveDamage(damage);
     }
 }
